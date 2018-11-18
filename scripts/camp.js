52311b71f630fdfcb5bc3133d98db2058dabd3b8
aengel88 <35244592+aengel88@users.noreply.github.com>
2018-11-18 10:02:24 -0800

    states and park buttons partial

--- scripts/camp.js
+++ scripts/camp.js
@@ -5,3 +5,15 @@ $('.book-now').click(function() {
 $('.cancel').click(function() {
 	$('.book-pop-up').toggleClass('book-pop');
 });
+
+$('.bs').click(function(){
+	window.location = 'index.html#map';
+});
+
+$('.bp').click(function() {
+	$('.parks-pop-up').toggleClass('park-pop');
+});
+
+$('.parks-cancel').click(function() {
+	$('.parks-pop-up').toggleClass('park-pop');
+});
